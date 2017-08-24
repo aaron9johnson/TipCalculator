00141917b4a769414fc5c8050f7920ce883ba55c
Aaron Johnson <aaron9johnson@gmail.com>
2017-08-23 22:06:08 -0700

    Initial Commit

---
+++ tipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tipCalculator
+//
+//  Created by Aaron Johnson on 2017-08-23.
+//  Copyright © 2017 Aaron Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
