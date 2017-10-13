515d37f7829d5f48157de02ebc1ea813b4b6fc32
Aaron Johnson <aaron9johnson@gmail.com>
2017-10-13 11:12:11 -0700

    did in pre course

--- tipCalculator/ViewController.m
+++ tipCalculator/ViewController.m
@@ -38,11 +38,18 @@
     [self calculateTip];
 }
 - (IBAction)calculateTip {
+    float num3 = [_tipPercentageTextField.text floatValue] * [_billAmountTextField.text floatValue] * 0.01;
+    num3 = (round (num3 * 100)) / 100;
+    float num4 = num3 + [_billAmountTextField.text floatValue];
+    _tipAmountLabel.numberOfLines = 0;
+    _tipAmountLabel.text = [NSString stringWithFormat:@"Tip amount:\t$%.02f \nTotal amount:\t$%.02f",num3, num4];
+   /*
     NSNumber *num = @([_tipPercentageTextField.text floatValue] * [_billAmountTextField.text floatValue] * 0.01);
     num = @((round ([num floatValue] * 100.0)) / 100.0); //rounds total to 2 decimal places >gives occasional float rounding errors
     NSNumber *num2 = @([_billAmountTextField.text floatValue] + [num floatValue]);
     _tipAmountLabel.numberOfLines = 0; //fixed text not showing up
     _tipAmountLabel.text = [NSString stringWithFormat:@"Tip amount:\t$%@ \nTotal amount:\t$%@",num, num2];
+    */
 }
 
 - (IBAction)adjustTipPercentage:(UISlider *)sender {
